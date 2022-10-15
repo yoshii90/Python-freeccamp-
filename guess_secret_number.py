e488a3479d2f7873068e1304277541f5b00451c2
yoshii90 <90890172+yoshii90@users.noreply.github.com>
2022-10-14 22:23:29 -0700

    Add files via upload

---
+++ guess_secret_number.py
@@ -0,0 +1,32 @@
+import random
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f'Guess a number between 1 and {x}:'))
+        if guess < random_number:
+            print("Sorry! Guess again. Too low.")
+        elif guess > random_number:
+            print("Sorry! Guess again. Too high.")
+
+    print(f'Congrats! The number is {random_number}!')
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low #could be also high becasue low=high
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)').lower()
+        if feedback =='h':
+            high = guess -1
+        elif feedback == 'l':
+            low = guess +1
+
+    print(f'Congrats! The computer guessed your number, {guess}, correctly!')
+
+computer_guess(1000)
